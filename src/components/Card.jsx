b129eeab447358deb8b903c462ae392b8875460e
Gerardo Keys <gerardokeys@Gerardos-MacBook-Pro.local>
2021-03-31 10:27:09 -0700

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,74 @@
-import React from 'react';
-import desktopWoman from '../images/illustration-woman-online-desktop.svg';
-import arrowdown from '../images/icon-arrow-down.svg';
+import React, { useState, useEffect } from "react";
+import desktopWoman from "../images/illustration-woman-online-desktop.svg";
+import arrow from "../images/icon-arrow-down.svg";
+
+import { data } from "../data.js";
 
 export default function Card() {
+  let element = [];
 
+  const [cardId, setCardId] = useState({
+    id: null,
+    open: false,
+  });
+
+  const iconArrowUp = (id) => {
     return (
-        <div className='card'>        
-            <img src={desktopWoman} alt="desktop-woman-icon" className='card__desktop-image-woman'/>
-            <div className='card__desktop-faq'>
-                <h1 className='card__desktop-faq--title'>FAQ</h1>
-                <div className='card__desktop-section-container'>
-                <p className='card__desktop-faq--information'>How many team members can I invite?</p>
-                <img src={arrowdown} alt="icon-arrow-down" className='card__desktop-icon-arrow'/>
-                </div>
-                <hr className='card__desktop-faq--divider'/>
-                <div className='card__desktop-section-container'>
-                <p className='card__desktop-faq--information'>How many team members can I invite?</p>
-                <img src={arrowdown} alt="icon-arrow-down" className='card__desktop-icon-arrow'/>
-                </div>
-                <hr className='card__desktop-faq--divider'/>
-                <div className='card__desktop-section-container'>
-                <p className='card__desktop-faq--information'>How many team members can I invite?</p>
-                <img src={arrowdown} alt="icon-arrow-down" className='card__desktop-icon-arrow'/>
-                </div>
-                <hr className='card__desktop-faq--divider'/>
-                <div className='card__desktop-section-container'>
-                <p className='card__desktop-faq--information'>How many team members can I invite?</p>
-                <img src={arrowdown} alt="icon-arrow-down" className='card__desktop-icon-arrow'/>
-                </div>
-                <hr className='card__desktop-faq--divider'/>
+      <img
+        src={arrow}
+        alt="icon-arrow-up"
+        className={"card__desktop-icon-arrow--two"}
+        id={id}
+        onClick={(e) => setCardId({ ...cardId, id: e.target.id, open: !cardId.open })}
+      />
+    );
+  };
+
+ 
+
+  const iconArrowDown = (id) => {
+    return (
+      <img
+        src={arrow}
+        alt="icon-arrow-down"
+        className={"card__desktop-icon-arrow"}
+        id={id}
+        onClick={(e) => setCardId({ ...cardId, id: e.target.id, open: !cardId.open })}
+      />
+    );
+  };
+
+  return (
+    <div className="card">
+      <img
+        src={desktopWoman}
+        alt="desktop-woman-icon"
+        className="card__desktop-image-woman"
+      />
+      <div className="card__desktop-faq">
+        <h1 className="card__desktop-faq--title">FAQ</h1>
+
+        {data.cards.map((data) => {
+          return (
+            <div key={data.id}>
+              <div className="card__desktop-section-container">
+                <p className="card__desktop-faq--information">
+                  {data.question}
+                </p>  
+                {
+                  data.id === cardId.id && cardId.open === true ?
+                  iconArrowDown(data.id) :
+                  iconArrowUp(data.id)
+                  
+                  
+                  
+                  }
+              </div>
+              <hr className="card__desktop-faq--divider" />
             </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import './fonts/KumbhSans-Bold.ttf';
+import './fonts/KumbhSans-Light.ttf';
+import './fonts/KumbhSans-Regular.ttf';
 import "./styles.scss";
 
 ReactDOM.render(
